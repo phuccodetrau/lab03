2a4f180d0c89d0662774de1ec4baefc32cc9ae89
Phuc <hoangphucythus@gmail.com>
2023-10-02 07:25:46 +0700

    method-overloading

--- AimsProject/src/Phuc/Cart.java
+++ AimsProject/src/Phuc/Cart.java
@@ -18,4 +18,21 @@ public class Cart {
 		}
 		return total;
 	}
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc [] discList) {
+		int n = discList.length;
+		for(int i = 0; i<n; i++) {
+			itemsOrdered[qtyOrdered ++] = discList[i];
+		}
+	}
+	
+	public void addDigitalVideoDisc(String title, String category, String director, int length, float cost) {
+		DigitalVideoDisc disc = new DigitalVideoDisc(title, category, director, length, cost);
+		itemsOrdered[qtyOrdered++] = disc;
+	}
+	
+	public void addDigitalVideoDisc(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
+		itemsOrdered[qtyOrdered++] = disc1;
+		itemsOrdered[qtyOrdered++] = disc2;
+	}
 }
