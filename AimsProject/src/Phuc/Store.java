c677939cc86dd27e4b5168f0134e3884e480c833
Phuc <hoangphucythus@gmail.com>
2023-10-03 21:09:00 +0700

    store

---
+++ AimsProject/src/Phuc/Store.java
@@ -0,0 +1,31 @@
+package Phuc;
+
+public class Store {
+
+	int quantity;
+	public DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[1000];
+	
+	
+	public Store() {
+		super();
+		quantity = 0;
+	}
+	public void addDVD(DigitalVideoDisc dvd) {
+		this.itemsInStore[quantity++] = dvd;
+	}
+	public void removeDVD(int index) {
+		for(int i = index; i<quantity; i++) {
+			itemsInStore[i] = itemsInStore[i+1];
+		}
+		itemsInStore[quantity] = null;
+		quantity --;
+	}
+	public void print() {
+		System.out.println("***********************SPORT***********************");
+		System.out.println("Ordered Items:");
+		for(int i = 0; i<quantity; i++) {
+			System.out.println((i+1) + ". " + itemsInStore[i].toString());
+		}
+		System.out.println("***************************************************");
+	}
+}
---
+++ AimsProject/src/Phuc/StoreTest.java
@@ -0,0 +1,24 @@
+package Phuc;
+
+public class StoreTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Store s = new Store();
+		
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		s.addDVD(dvd1);
+		
+		DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		s.addDVD(dvd2);
+		
+		DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+		s.addDVD(dvd3);
+		
+		s.print();
+		
+		s.removeDVD(1);
+		s.print();
+	}
+
+}
